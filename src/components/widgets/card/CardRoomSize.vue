dbe58ffc82a431a38c76890bd434fb3da142cf98
davidHeymann <heymann.david@gmail.com>
2020-07-17 13:48:38 +0300

    ref<size range>: Add size of people

--- src/components/widgets/card/CardRoomSize.vue
+++ src/components/widgets/card/CardRoomSize.vue
@@ -53,6 +53,26 @@
             text: '5',
             value: 5,
           },          
+          {
+            text: '6',
+            value: 6,
+          },
+          {
+            text: '7',
+            value: 7,
+          },
+          {
+            text: '8',
+            value: 8,
+          },
+          {
+            text: '9',
+            value: 9,
+          },
+          {
+            text: '10',
+            value: 10,
+          },
         ],
       }
     },
